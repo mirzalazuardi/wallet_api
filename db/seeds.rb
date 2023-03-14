ddba551058e4cab3a1a4777ab6a1d5cd00a52600
Mirzalazuardi Hermawan <mirzalazuardi@users.noreply.github.com>
2023-03-15 00:21:09 +0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(email: 'user@wallet.app', password: '12345678')
+u1 = User.create(email: 'user1@wallet.app', password: '12345678')
+u2 = User.create(email: 'user2@wallet.app', password: '12345678')
+w1 = Wallet.create(ownable: u1, balance: 20)
+w2 = Wallet.create(ownable: u2, balance: 30)
