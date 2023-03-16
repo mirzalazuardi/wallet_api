d6f3c45c489e71c6e090b457ceae9bd9b8a82707
Mirzalazuardi Hermawan <mirzalazuardi@users.noreply.github.com>
2023-03-16 14:06:21 +0700

    add authenticate and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_user
+
+  def authenticate
+    session_obj = Session.find_by(token: (request.headers['token']))
+    return render json: { data: 'invalid auth'}, status: :unauthorized if session_obj.blank?
+    @current_user = User.find(session_obj.user_id)
+  end
 end
