8bf2d870e756e196e268ae19b079d805d391acb0
Mirzalazuardi Hermawan <mirzalazuardi@users.noreply.github.com>
2023-03-16 17:31:20 +0700

    add class method on stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,18 @@
 class Stock < ApplicationRecord
   has_one :wallet, as: :ownable
   has_many :stock_prices
+
+  def self.latest_stock(identifier)
+    JSON.parse(LatestStockPrice.new('any', "Identifier=#{identifier}").exec)
+  end
+
+  def self.parse_stock(hash_data)
+    {}.tap do |h|
+      h[:identifier] = hash_data['identifier']
+      h[:symbol] = hash_data['symbol']
+      h[:price] = hash_data['lastPrice']
+      h[:update_time] = DateTime.strptime(hash_data['lastUpdateTime'], '%d-%b-%Y %H:%M:%S')
+    end
+  end
+
 end
